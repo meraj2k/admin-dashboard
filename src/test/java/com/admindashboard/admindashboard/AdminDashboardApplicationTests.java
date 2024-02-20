67c8d7c5a3511786b5ca29f8b1147536333ee8a1
Meraj Ansari <merajansari@Merajs-MacBook-Air.local>
2024-02-21 00:08:27 +0530

    first commit

---
+++ src/test/java/com/admindashboard/admindashboard/AdminDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admindashboard.admindashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
